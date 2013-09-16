c8976acff5b68670f2783f4d729c0d808e6d70ac
Neil Xavier D'Souza <nxd_in@yahoo.com>
2013-09-16 18:53:28 +0530

    switching branches for next merge - some forgotten files earlier

--- xtcc/qscript/stubs/simple_compiler/runtime/cpp-emscripten/dom_manip_funcs.h
+++ xtcc/qscript/stubs/simple_compiler/runtime/cpp-emscripten/dom_manip_funcs.h
@@ -7,6 +7,7 @@ void print_to_question_area(const char * question_text_ptr);
 void print_to_stub_area (const char * question_type, int no_mpn, const char * stub_info, int counter);
 void save_qnre_data(char * qnre_data);
 void show_end_of_qnre_page();
+void create_question_form (char * question_json_ptr, char * stubs_json_ptr);
 //void print_to_question_response_area(const char * question_text_ptr);
 
 }
